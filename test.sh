853eb99b0e43d7ee66547adf095756467849705a
Kobayashi82 <kobayashi82@outlook.com>
2025-05-27 22:41:32 +0200

    modificado test.sh de la raiz

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@
 # -lft_malloc				= -l busca lib + ft_malloc + .so
 # -Wl,-rpath=./build/lib	= Pasa al linker el parametro rpath para que busque en esa ruta las bibliotecas en runtime
 
+echo
 gcc -g -Wno-free-nonheap-object -o test test.c -I./inc -L./build/lib -lft_malloc -Wl,-rpath=./build/lib -pthread
 ./test
+rm test
+echo
