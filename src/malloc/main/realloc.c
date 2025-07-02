77868c884618c34bcfbd09a14c87d23d79ca4485
Kobayashi82 <kobayashi82@outlook.com>
2025-07-02 19:48:23 +0200

    Parece que el realloc está hecho por fin

--- src/malloc/main/realloc.c
+++ src/malloc/main/realloc.c
@@ -6,7 +6,7 @@
 /*   By: vzurera- <vzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 11:32:56 by vzurera-          #+#    #+#             */
-/*   Updated: 2025/07/02 16:32:07 by vzurera-         ###   ########.fr       */
+/*   Updated: 2025/07/02 19:44:02 by vzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,6 @@
 
 		mutex(&tcache->mutex, MTX_LOCK);
 
-			// tiene que buscar en todas las arenas
 			heap = heap_find(tcache, ptr);
 			if (!heap) {
 				mutex(&tcache->mutex, MTX_UNLOCK);
@@ -112,8 +111,6 @@
 				mutex(&tcache->mutex, MTX_UNLOCK);
 				return (NULL);
 			}
-
-			size_t size_aligned = ALIGN(size + sizeof(t_chunk));
 
 			t_chunk *chunk = GET_HEAD(ptr);
 			size_t	chunk_size = GET_SIZE(chunk);
@@ -131,18 +128,78 @@
 						SET_PREV_SIZE(next_chunk, remaining - sizeof(t_chunk));
 						next_chunk->size &= ~PREV_INUSE;
 						link_chunk(new_chunk, remaining - sizeof(t_chunk), heap->type, tcache, heap);
+						heap->free += remaining;
 						new_ptr = ptr;
 					} else new_ptr = ptr;
 				}
 			} else if (heap->type != LARGE) {
-				// extend
+				size_t needed_size = ALIGN(size);
+				size_t current_size = GET_SIZE(chunk);
+
+				if (needed_size > current_size) {
+					size_t extra_needed = needed_size - current_size;
+					size_t absorbed = 0;
+					t_chunk *next = GET_NEXT(chunk);
+					bool can_extend = false;
+					
+					if (needed_size < ((heap->type == TINY) ? TINY_CHUNK : SMALL_CHUNK)) {
+						while (next && absorbed < extra_needed) {
+							if (!IS_TOPCHUNK(next) && !IS_FREE(next)) break;
+
+							if (IS_TOPCHUNK(next)) {
+								next = split_top_chunk(heap, ALIGN((extra_needed - absorbed) + sizeof(t_chunk)));
+								if (next) {
+									absorbed += GET_SIZE(next) + sizeof(t_chunk);
+									can_extend = true;
+									heap->free -= GET_SIZE(next) + sizeof(t_chunk);
+								}
+								break;
+							} else {
+								unlink_chunk(next, tcache, heap);
+								absorbed += GET_SIZE(next) + sizeof(t_chunk);
+								if (absorbed >= extra_needed) { can_extend = true; break; }
+								heap->free_chunks--;
+								heap->free -= GET_SIZE(next) + sizeof(t_chunk);
+								next = GET_NEXT(next);
+							}
+						}
+
+						if (can_extend && absorbed >= extra_needed) {
+
+							chunk->size = (chunk->size & (HEAP_TYPE | PREV_INUSE)) | (current_size + absorbed);
+
+							if (current_size + absorbed > ALIGN(size)) {
+								size_t remaining = (current_size + absorbed) - ALIGN(size);
+								if (remaining >= sizeof(t_chunk) + ((heap->type == TINY) ? 48 : TINY_CHUNK)) {
+									chunk->size = (chunk->size & (HEAP_TYPE | PREV_INUSE)) | ALIGN(size);
+									t_chunk *new_chunk = (t_chunk *)((char *)chunk + ALIGN(size) + sizeof(t_chunk));
+									SET_PREV_SIZE(new_chunk, ALIGN(size));
+									SET_POISON(GET_PTR(new_chunk));
+									new_chunk->size = (remaining - sizeof(t_chunk)) | ((heap->type == SMALL) ? HEAP_TYPE : 0) | PREV_INUSE;
+									t_chunk *next_chunk = GET_NEXT(new_chunk);
+									SET_PREV_SIZE(next_chunk, remaining - sizeof(t_chunk));
+									next_chunk->size &= ~PREV_INUSE;
+									link_chunk(new_chunk, remaining - sizeof(t_chunk), heap->type, tcache, heap);
+									heap->free += remaining;
+								}
+							}
+						
+							t_chunk *new_next = GET_NEXT(chunk);
+							if (new_next) {
+								SET_PREV_SIZE(new_next, GET_SIZE(chunk));
+								new_next->size |= PREV_INUSE;
+							}
+
+							new_ptr = ptr;
+						}
+					}
+				} else new_ptr = ptr;
 			}
 
 		mutex(&tcache->mutex, MTX_UNLOCK);
 
 		if (!new_ptr) {
-			// Si no se puede expandir
-			new_ptr = allocate("REALLOC", size_aligned, 0);
+			new_ptr = allocate("REALLOC", ALIGN(size + sizeof(t_chunk)), 0);
 			if (new_ptr) {
 				is_new = true;
 				size_t copy_size = GET_SIZE((t_chunk *)GET_HEAD(ptr));
