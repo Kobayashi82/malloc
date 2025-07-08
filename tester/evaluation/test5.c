23b7a1cea7612f35f002d99baa8144cbb7670da0
Kobayashi82 <Kobayashi82@outlook.com>
2025-07-08 10:03:01 +0200

    Un par de cositas

--- tester/evaluation/test5.c
+++ tester/evaluation/test5.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void *show_alloc_mem_ex(void *ptr, size_t offset, size_t length);
 void *show_alloc_mem();
@@ -12,6 +13,7 @@ int main()
 	malloc(1024 * 1024 * 16);
 	malloc(1024 * 1024 * 128);
 	show_alloc_mem();
+	write (2, "\n\n", 2);
 	char *ptr = malloc(30);
 	strcpy(ptr, "Hello World!\n");
 	show_alloc_mem_ex(ptr, 0, 0);
